b0f7e6ffb84827ed4f17176b95b2055ee0536220
yunben <vietzz771@gmail.com>
2024-06-10 23:35:30 +0700

    change import url

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 import connectDB from "./config/db.js";
 import cors from "cors";
 import { errorResponserHandler, invalidPathHandler } from "./middleware/errorHandler.js";
